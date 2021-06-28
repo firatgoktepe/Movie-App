a1dfdd6e21d294075f4fbf40ba1710442c491601
Firat Göktepe <firat@naxnordic.com>
2021-06-28 15:15:43 +0300

    files

---
+++ src/js/base.js
@@ -0,0 +1,81 @@
+export const elements = {
+    searchForm: document.getElementById('form-search'),
+    searchInput: document.getElementById('txt-keyword'),
+    movieListHeader: document.getElementById('movie-list-header'),
+    movieList: document.getElementById('movie-list'),
+    movieListContainer: document.getElementById('movie-list-container'),
+    movieDetails: document.getElementById('movie-details'),
+    movieDetailsContainer: document.getElementById('movie-details-container'),
+    movieDetailsClose: document.getElementById('movie-details-close'),
+}
+export const noResult = parent => {
+  const alert = `<div class="alert-msg" style="color: red;">No Result!</div>`;
+  parent.insertAdjacentHTML('beforebegin', alert);
+}
+
+export const clearNoResult = parent => {
+  const alert = parent.previousSibling;
+  if(alert)
+    alert.parentNode.removeChild(alert);
+}
+
+export const alert_msg = parent => {
+  const alert = `<div class="alert-msg" style="color: red;">Please type a movie you want to search!</div>`;
+  parent.insertAdjacentHTML('beforebegin', alert);
+}
+
+export const clearAlert = parent => {
+  const alert = parent.previousSibling;
+  if(alert)
+    alert.parentNode.removeChild(alert);
+}
+
+export const renderLoader = parent => {
+  const loader = `<div class="nb-spinner"></div>`;
+  parent.insertAdjacentHTML('beforebegin', loader);
+}
+
+export const clearLoader = parent => {
+  const loader = parent.previousSibling;
+  if(loader)
+    loader.parentNode.removeChild(loader);
+}
+
+export const image_sizes = {
+    backdrop_sizes: [
+        "w300",
+        "w780",
+        "w1280",
+        "original"
+      ],
+      logo_sizes: [
+        "w45",
+        "w92",
+        "w154",
+        "w185",
+        "w300",
+        "w500",
+        "original"
+      ],
+      poster_sizes: [
+        "w92",
+        "w154",
+        "w185",
+        "w342",
+        "w500",
+        "w780",
+        "original"
+      ],
+      profile_sizes: [
+        "w45",
+        "w185",
+        "h632",
+        "original"
+      ],
+      still_sizes: [
+        "w92",
+        "w185",
+        "w300",
+        "original"
+      ]
+}
---
+++ src/js/config.js
@@ -0,0 +1,2 @@
+export const api_key = "a02b44460a0047d5300ca41a062e7aae";
+export const base_url = "https://api.themoviedb.org/3"
